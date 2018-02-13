56041e82a21e1c25aa94e284f4bdfcea5e0bf678
EliLazarus <lazarus.eli@gmail.com>
2018-02-13 13:57:10 -0800

    Added relative file path and histogram slider updating

--- GFN_Data_Visualization/ScatterVisuals/app.R
+++ GFN_Data_Visualization/ScatterVisuals/app.R
@@ -10,8 +10,8 @@
 library(shiny)
 
 # Reading in the CLUM Data
-CLUMData <- read.csv("/Users/scottkaplan1112/Box Sync/Graduate School/A_DS421/Spring 2018 Project/EnergyEcoGroup_FinalProject/GFN_Data_Visualization/NFA_2017_CLUM.csv")
-
+#CLUMData <- read.csv("/Users/scottkaplan1112/Box Sync/Graduate School/A_DS421/Spring 2018 Project/EnergyEcoGroup_FinalProject/GFN_Data_Visualization/NFA_2017_CLUM.csv")
+CLUMData <- read.csv("../NFA_2017_CLUM.csv")
 # Define UI for application that draws a histogram
 ui <- pageWithSidebar(
   headerPanel('Iris k-means clustering'),
