49a4080e2f98f80c1ae09928bb10b45eecb8c5b3
EliLazarus <lazarus.eli@gmail.com>
2018-02-21 22:12:41 -0800

    Added zoomed plot and hover labels. Moved log conversion to selected, but not implemented yet.

--- GFN_Data_Visualization/ScatterVisuals/app.R
+++ GFN_Data_Visualization/ScatterVisuals/app.R
@@ -10,44 +10,73 @@
 library(shiny)
 library(data.table)
 
+
 # Reading in the CLUM Data
 #CLUMData <- read.csv("/Users/scottkaplan1112/Box Sync/Graduate School/A_DS421/Spring 2018 Project/EnergyEcoGroup_FinalProject/GFN_Data_Visualization/NFA_2017_CLUM.csv")
 CLUMData <- read.csv("NFA_2017_CLUM.csv")
 
 # Taking logs of all of the variables for scaling
 cols <- c(names(CLUMData[,6:13]))
-CLUMData[cols] <- log(CLUMData[cols])
-setnames(CLUMData, old = c(names(CLUMData[,6:13])), new = c("Log Coicop Expenditure", "Log Crop-Land", "Log Grazing-Land", "Log Forest-Land", "Log Fishing-Ground", "Log BuiltUp-Land", "Log Carbon", "Log Total"))
+CLUMData[cols] <- (CLUMData[cols])
+# Just the country and year for hover
+CLUMData_CountryYear <- CLUMData[,c("year","GTAP_name")]
+setnames(CLUMData, old = c(names(CLUMData[,6:13])), new = c("Coicop Expenditure", "Crop-Land", 
+                                                            "Grazing-Land", "Forest-Land", "Fishing-Ground",
+                                                            "BuiltUp-Land", "Carbon", "Total"))
 
 # Define UI for application that draws a histogram
 ui <- pageWithSidebar(
   headerPanel('Initial Visual Exploration of CLUM Data'),
-  sidebarPanel(
+  sidebarPanel(width = 3,
+    #selection of GTAP years, 2011 as default
+    checkboxGroupInput("Select_years", "Years", unique(CLUMData[,1]), selected = "2011", FALSE),
     selectInput('xcol', 'X Variable', names(CLUMData[,6:13])),
-    selectInput('ycol', 'Y Variable', names(CLUMData[,6:13]), selected = "Log Crop-Land"),
+    selectInput('ycol', 'Y Variable', names(CLUMData[,6:13]), selected = "Total"),
     selectInput('zcol', 'Z Variable', unique(CLUMData[,5]),
                 selected=names(CLUMData)[[2]]),
+    selectInput('scale', 'Scale', c("normal","log"), selected="normal"),
     numericInput('clusters', 'Cluster count', 3,
                  min = 1, max = 9)
   ),
   mainPanel(
-    plotOutput('plot1')
+    fluidRow(
+     column(width = 6, class = "well",
+            h4("Selection controls right plot"),
+            
+      plotOutput('plot1', hover = "plot_hover",
+                 brush = brushOpts(
+                   id = "plot_brush",
+                   resetOnNew = TRUE)
+       )
+     ),
+     column(width = 6, class = "well",
+            h4("Zoomed from selection"),
+            plotOutput("plot2")
+     )
+    ),
+
+    fluidRow(
+     column(width = 12,
+      h6("Hover IDs"),
+      verbatimTextOutput("info")
+      )
+    )
   )
 )
-
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
   # Combine the selected variables into a new data frame
   selectedData <- reactive({
-    CLUMData_subset <- subset(CLUMData, clum7_name==input$zcol)
-    CLUMData_subset[, c(input$xcol, input$ycol)]
+    CLUMData_subset <- subset(CLUMData, clum7_name==input$zcol & year %in% input$Select_years)
+    CLUMData_subset[,c(input$xcol, input$ycol)]
   })
   
   clusters <- reactive({
     kmeans(selectedData(), input$clusters)
   })
   
+  ranges <- reactiveValues(x = NULL, y = NULL)
   
   output$plot1 <- renderPlot({
     palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
@@ -58,8 +87,38 @@ server <- function(input, output, session) {
          col = clusters()$cluster,
          pch = 20, cex = 3)
     points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    
+  })
+    
+   output$plot2 <- renderPlot({
+     palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+               "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+     
+     par(mar = c(5.1, 4.1, 0, 1))
+     plot(selectedData(), xlim=c(input$plot_brush$xmin, input$plot_brush$xmax), 
+          ylim=c(input$plot_brush$ymin, input$plot_brush$ymax),
+          col = clusters()$cluster,
+          pch = 20, cex = 3)
+     points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+   })
+# When a double-click happens, check if there's a brush on the plot.
+# If so, zoom to the brush bounds; if not, reset the zoom.
+  observe({
+    brush <- input$plot_brush
+    if (!is.null(brush)) {
+      ranges$x <- c(brush$xmin, brush$xmax)
+      ranges$y <- c(brush$ymin, brush$ymax)
+  
+    } else {
+      ranges$x <- NULL
+      ranges$y <- NULL
+    }
   })
   
+  output$info <- renderPrint({
+    #It would be good to suppress the text when nothing near and suppress line number
+     nearPoints(CLUMData, input$plot_hover, xvar = input$xcol, yvar = input$ycol, addDist = FALSE)[c(1,3)]  
+  })
 }
 
 # Run the application 
