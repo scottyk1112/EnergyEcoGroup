f4685d46a0675621a18151e8c7b67b9f58e38089
Scott Kaplan <scottkaplan@berkeley.edu>
2018-04-18 15:43:02 -0700

    new updates

--- World Bank Data/World Bank Data Pull Script.R
+++ World Bank Data/World Bank Data Pull Script.R
@@ -11,40 +11,40 @@ IndicatorList <- as.data.frame(IndicatorAndCountries[[1]])
 
 
 ######Food Section
-FoodDeficit <- "SN.ITK.DFCT"
-CerealYield <- "AG.YLD.CREL.KG"
-AgVAPerWorker <- "EA.PRD.AGRI.KD"
-AgVA <- "EA.PRD.LAND.KD"
-FertilizerCons <- "AG.CON.FERT.ZS"
-FoodProdIndex <- "AG.PRD.FOOD.XD"
-TractorPer100SqKm <- "AG.LND.TRAC.ZS" 
-AgIrrigatedLand <- "AG.LND.IRIG.AG.ZS"
-FishSpeciesThreatened <- "EN.FSH.THRD.NO"
+FoodDeficit <- "SN.ITK.DFCT" #High is bad, low is good#
+CerealYield <- "AG.YLD.CREL.KG" #High is good, low is bad#
+AgVAPerWorker <- "EA.PRD.AGRI.KD" #High is good, low is bad#
+AgVA <- "EA.PRD.LAND.KD" #High is good, low is bad#
+FertilizerCons <- "AG.CON.FERT.ZS" #High is good, low is bad#
+FoodProdIndex <- "AG.PRD.FOOD.XD" #High is good, low is bad#
+TractorPer100SqKm <- "AG.LND.TRAC.ZS"  #High is good, low is bad#
+AgIrrigatedLand <- "AG.LND.IRIG.AG.ZS" #High is good, low is bad#
+FishSpeciesThreatened <- "EN.FSH.THRD.NO" #High is bad, low is good#
 
 Food_Indicators <- c(FoodDeficit, CerealYield, AgVAPerWorker, AgVA, FertilizerCons, 
                      FoodProdIndex, TractorPer100SqKm, AgIrrigatedLand, FishSpeciesThreatened)
 
 ######Government Section
-SchoolEnrollment_GPI <- "SE.ENR.PRSC.FM.ZS"
-SanitationAccess <- "SH.STA.BASS.ZS"
-CentralGovtDebt <- "GC.DOD.TOTL.GD.ZS"
-FDI_NetInflows <- "BN.KLT.DINV.DRS.GDP.ZS"
-GrossSavings <- "NY.GNS.ICTR.ZS"
-CurrentAccountBal <- "BN.CAB.XOKA.GD.ZS"
-EducationExpend <- "SE.XPD.TOTL.GB.ZS"
-ExpendPerStudent <- "UIS.XUNIT.PPPCONST.1.FSGOV"
-TeachersPrimaryEd_Trained <- "SE.PRM.TCAQ.ZS"
-Unemployment <- "SL.UEM.TOTL.NE.ZS"
-NetDevAssistanceReceived <- "DT.ODA.ODAT.KD"
-Inflation <- "FP.CPI.TOTL.ZG"
-DomesticCredit_PrivateSector <- "FS.AST.PRVT.GD.ZS"
-HealthExpend_Total <- "SH.XPD.PUBL.ZS"
-CPIAEconManageClusterAvg <- "IQ.CPA.ECON.XQ"
-CPIAPublicManageClusterAvg <- "IQ.CPA.PUBS.XQ"
-IDAResourceAllocIndex <- "IQ.CPA.IRAI.XQ"
-PropSeatsWomenParliament <- "SG.GEN.PARL.ZS"
-CPIASocialInclusion <- "IQ.CPA.SOCI.XQ"
-CPIAStructuralPolicies <- "IQ.CPA.STRC.XQ"
+SchoolEnrollment_GPI <- "SE.ENR.PRSC.FM.ZS" #High is good, low is bad#
+SanitationAccess <- "SH.STA.BASS.ZS" #High is good, low is bad#
+CentralGovtDebt <- "GC.DOD.TOTL.GD.ZS" #High is bad, low is good#
+FDI_NetInflows <- "BN.KLT.DINV.DRS.GDP.ZS" #High is good, low is bad#
+GrossSavings <- "NY.GNS.ICTR.ZS" #High is good, low is bad#
+CurrentAccountBal <- "BN.CAB.XOKA.GD.ZS" #High is bad, low is good#
+EducationExpend <- "SE.XPD.TOTL.GB.ZS" #High is good, low is bad#
+ExpendPerStudent <- "UIS.XUNIT.PPPCONST.1.FSGOV" #High is good, low is bad#
+TeachersPrimaryEd_Trained <- "SE.PRM.TCAQ.ZS" #High is good, low is bad#
+Unemployment <- "SL.UEM.TOTL.NE.ZS" #High is bad, low is good#
+NetDevAssistanceReceived <- "DT.ODA.ODAT.KD" #High is bad, low is good#
+Inflation <- "FP.CPI.TOTL.ZG" #High is bad, low is good#
+DomesticCredit_PrivateSector <- "FS.AST.PRVT.GD.ZS" #High is good, low is bad#
+HealthExpend_Total <- "SH.XPD.PUBL.ZS" #High is good, low is bad#
+CPIAEconManageClusterAvg <- "IQ.CPA.ECON.XQ" #High is good, low is bad#
+CPIAPublicManageClusterAvg <- "IQ.CPA.PUBS.XQ" #High is good, low is bad#
+IDAResourceAllocIndex <- "IQ.CPA.IRAI.XQ" #High is good, low is bad#
+PropSeatsWomenParliament <- "SG.GEN.PARL.ZS" #High is good, low is bad#
+CPIASocialInclusion <- "IQ.CPA.SOCI.XQ" #High is good, low is bad#
+CPIAStructuralPolicies <- "IQ.CPA.STRC.XQ" #High is good, low is bad#
   
 Government_Indicators <- c(SchoolEnrollment_GPI, SanitationAccess, CentralGovtDebt, FDI_NetInflows,
                            GrossSavings, CurrentAccountBal, EducationExpend, ExpendPerStudent,
@@ -55,22 +55,22 @@ Government_Indicators <- c(SchoolEnrollment_GPI, SanitationAccess, CentralGovtDe
 
 
 ##Services Metrics
-HospitalBeds <- "SH.MED.BEDS.ZS"
-TelephoneSubscriptions <- "IT.MLT.MAIN.P2"
-BroadbandSubscriptions <- "IT.NET.BBND.P2"
-NetEnrollmentRate <- "SE.PRM.NENR"
-TransitionRate_PrimarytoSecondary <- "SE.SEC.PROG.ZS"
-Persistence_LastGradePrimary <- "SE.PRM.PRSL.ZS"
-PrePrimaryEducation_Duration <- "SE.PRE.DURS"
-Pupil_Teacher_Ratio_PrePrimary <- "SE.PRE.ENRL.TC.ZS"
-Pupil_Teacher_Ratio_Primary <- "SE.PRM.ENRL.TC.ZS"
-TrainedTeachers_PrePrimary <- "SE.PRE.TCAQ.ZS"
-CPIA_SocialProtection <- "IQ.CPA.PROT.XQ"
-SocialProtectionAdequacy <- "per_allsp.adq_pop_tot"
-SocialInsuranceAdequacy <- "per_si_allsi.adq_pop_tot"
-Coverage_SocialInsurance <- "per_si_allsi.cov_pop_tot"
-Coverage_SocialInsurance_LowestQuintile <- "per_si_allsi.cov_ep_tot"
-CPIA_FinancialSector <- "IQ.CPA.FINS.XQ"
+HospitalBeds <- "SH.MED.BEDS.ZS" #High is good, low is bad#
+TelephoneSubscriptions <- "IT.MLT.MAIN.P2" #High is good, low is bad#
+BroadbandSubscriptions <- "IT.NET.BBND.P2" #High is good, low is bad#
+NetEnrollmentRate <- "SE.PRM.NENR" #High is good, low is bad#
+TransitionRate_PrimarytoSecondary <- "SE.SEC.PROG.ZS" #High is good, low is bad#
+Persistence_LastGradePrimary <- "SE.PRM.PRSL.ZS" #High is good, low is bad#
+PrePrimaryEducation_Duration <- "SE.PRE.DURS" #High is good, low is bad#
+Pupil_Teacher_Ratio_PrePrimary <- "SE.PRE.ENRL.TC.ZS" #High is bad, low is good#
+Pupil_Teacher_Ratio_Primary <- "SE.PRM.ENRL.TC.ZS" #High is bad, low is good#
+TrainedTeachers_PrePrimary <- "SE.PRE.TCAQ.ZS" #High is good, low is bad#
+CPIA_SocialProtection <- "IQ.CPA.PROT.XQ" #High is good, low is bad#
+SocialProtectionAdequacy <- "per_allsp.adq_pop_tot" #High is good, low is bad#
+SocialInsuranceAdequacy <- "per_si_allsi.adq_pop_tot" #High is good, low is bad#
+Coverage_SocialInsurance <- "per_si_allsi.cov_pop_tot" #High is good, low is bad#
+Coverage_SocialInsurance_LowestQuintile <- "per_si_allsi.cov_ep_tot" #High is good, low is bad#
+CPIA_FinancialSector <- "IQ.CPA.FINS.XQ" #High is good, low is bad#
   
 Services_Indicators <- c(HospitalBeds, TelephoneSubscriptions, BroadbandSubscriptions, NetEnrollmentRate,
                          TransitionRate_PrimarytoSecondary, Persistence_LastGradePrimary, 
@@ -81,32 +81,37 @@ Services_Indicators <- c(HospitalBeds, TelephoneSubscriptions, BroadbandSubscrip
 
 
 ######Personal Transportation Section
-SchoolEnrollment_GPI <- "SE.ENR.PRSC.FM.ZS"
-#UrbanRoadDensity <- "IN.TRANSPORT.URBNRD.DENSIT"
-#RuralRoadDensity <- "IN.TRANSPORT.RURLRD.DENSIT"
-#RuralAccessRoads <- "IS.ROD.ALLS.ZS"
-RailPassengers <- "IS.RRS.PASG.KM" #Need to normalize this by population and country size somehow
-#RailPassengers_2 <- "IS.RRS.PASG.K2.PP.ZS" #Wasn't able to download
-RoadPassengers <- "IS.ROD.PSGR.K6" #Need to normalize this by population and country size somehow
-AirPassengers <- "IS.AIR.PSGR"     #Need to normalize this by population and country size somehow (& this is probably a lot of tourists!)
-#DeathsInTraffic <- "H.STA.TRAF.P5" #Wasn't able to download
+SchoolEnrollment_GPI <- "SE.ENR.PRSC.FM.ZS" #High is good, low is bad#
+UrbanRoadDensity <- "IN.TRANSPORT.URBNRD.DENSIT" #High is good, low is bad#
+RuralRoadDensity <- "IN.TRANSPORT.RURLRD.DENSIT" #High is good, low is bad#
+RuralAccessRoads <- "IS.ROD.ALLS.ZS" #High is good, low is bad#
+RailPassengers <- "IS.RRS.PASG.KM" #High is good, low is bad# #Need to normalize this by population and country size somehow
+RailPassengers_2 <- "IS.RRS.PASG.K2.PP.ZS" #High is good, low is bad# #Wasn't able to download
+RoadPassengers <- "IS.ROD.PSGR.K6" #High is good, low is bad# #Need to normalize this by population and country size somehow
+AirPassengers <- "IS.AIR.PSGR"    #High is good, low is bad#  #Need to normalize this by population and country size somehow (& this is probably a lot of tourists!)
+DeathsInTraffic <- "H.STA.TRAF.P5" #High is bad, low is good# #Wasn't able to download
 
-Transport_Indicators <- c(SchoolEnrollment_GPI, RailPassengers, RoadPassengers, AirPassengers)
+#Transport_Indicators <- c(SchoolEnrollment_GPI, UrbanRoadDensity, RuralRoadDensity, AirPassengers,
+#RuralAccessRoads, RailPassengers, RailPassengers_2, RoadPassengers, DeathsInTraffic)
+
+Transport_Indicators <- c(SchoolEnrollment_GPI, AirPassengers)
 
 
 ######Housing Section
-HHwithonsiteh2o <- "SG.H2O.PRMS.HH.ZS"
-H20Shortagepermo <- "IC.FRM.INFRA.IN6"
-DistOfHausByAvailOfH2o <- "IN.POV.HH.DRKNGWATER.WITHIN" # Not sure what this looks like "distribution?"
-HoursofPowerOutage <- "IC.ELC.OUTG.HR"
-PersonalToilet <- "SH.STA.SMSS.ZS"
-BldgQualityControlIndex <- "IC.DCP.BQCI"
-CookInHaus <- "SG.COK.HOUS.ZS"
-CookWithElec <- "SG.COK.ELEC.ZS"
-CookWithGas <- "SG.COK.LPGN.ZS"
+HHwithonsiteh2o <- "SG.H2O.PRMS.HH.ZS" #High is good, low is bad#
+H20Shortagepermo <- "IC.FRM.INFRA.IN6" #High is bad, low is good#
+DistOfHausByAvailOfH2o <- "IN.POV.HH.DRKNGWATER.WITHIN" #High is good, low is bad# # Not sure what this looks like "distribution?"
+HoursofPowerOutage <- "IC.ELC.OUTG.HR" #High is bad, low is good#
+PersonalToilet <- "SH.STA.SMSS.ZS" #High is good, low is bad#
+BldgQualityControlIndex <- "IC.DCP.BQCI" #High is good, low is bad#
+CookInHaus <- "SG.COK.HOUS.ZS" #High is good, low is bad#
+CookWithElec <- "SG.COK.ELEC.ZS" #High is good, low is bad#
+CookWithGas <- "SG.COK.LPGN.ZS" #High is good, low is bad#
 
-Housing_Indicators <- c(HHwithonsiteh2o, H20Shortagepermo, DistOfHausByAvailOfH2o, HoursofPowerOutage, 
-                        PersonalToilet, BldgQualityControlIndex, CookInHaus, CookWithElec, CookWithGas)
+#Housing_Indicators <- c(HHwithonsiteh2o, H20Shortagepermo, DistOfHausByAvailOfH2o, HoursofPowerOutage, 
+                        #PersonalToilet, BldgQualityControlIndex, CookInHaus, CookWithElec, CookWithGas)
+
+Housing_Indicators <- c(PersonalToilet)
 
 ##Goods Metrics (Prelminary) 
 # I'm tempted to use just 1 or two measures of material satisfaction and well-being
@@ -136,6 +141,31 @@ Transport_Data <- WB_DataPull_Function(Transport_Indicators, 2004, 2007, 2011)
 Housing_Data <- WB_DataPull_Function(Housing_Indicators, 2004, 2007, 2011)
 GoodsData <- read.csv("/Users/scottkaplan1112/Box Sync/Graduate School/A_DS421/Spring 2018 Project/EnergyEcoGroup_FinalProject/World Bank Data/ass_pov_final.csv")
 
+
+####Indices Reversal (Manual)
+
+##Food
+Food_Data$SN.ITK.DFCT <- Food_Data$SN.ITK.DFCT/100
+Food_Data$EN.FSH.THRD.NO <- Food_Data$EN.FSH.THRD.NO/100
+
+##Government
+Government_Data$GC.DOD.TOTL.GD.ZS <- Government_Data$GC.DOD.TOTL.GD.ZS/100
+Government_Data$BN.CAB.XOKA.GD.ZS <- Government_Data$BN.CAB.XOKA.GD.ZS/100
+Government_Data$SL.UEM.TOTL.NE.ZS <- Government_Data$SL.UEM.TOTL.NE.ZS/100
+Government_Data$DT.ODA.ODAT.KD <- Government_Data$DT.ODA.ODAT.KD/100
+Government_Data$FP.CPI.TOTL.ZG <- Government_Data$FP.CPI.TOTL.ZG/100
+
+##Services
+Services_Data$SE.PRE.ENRL.TC.ZS <- Services_Data$SE.PRE.ENRL.TC.ZS/100
+Services_Data$SE.PRM.ENRL.TC.ZS <- Services_Data$SE.PRM.ENRL.TC.ZS/100
+
+##Transport (none)
+
+##Housing (none)
+
+##Goods (none)
+
+
 ##NA Removal Function
 NARemove_Fun <- function(data, NA_factor){
   
@@ -157,7 +187,7 @@ NARemove_Fun <- function(data, NA_factor){
 FoodData_NoNAs <- NARemove_Fun(Food_Data, 2)
 GovernmentData_NoNAs <- NARemove_Fun(Government_Data, 2)
 ServicesData_NoNAs <- NARemove_Fun(Services_Data, 2)
-TransportData_NoNAs <- NARemove_Fun(Transport_Data, 1.5)
+TransportData_NoNAs <- NARemove_Fun(Transport_Data, 2)
 HousingData_NoNAs <- NARemove_Fun(Housing_Data, 1.25)
 
 
@@ -177,8 +207,9 @@ colnames(datamatrix)[ncol(datamatrix)] <- "MaxMin_Index"
 datamatrix$CLUM_category <- category
 
 datamatrix <- cbind(data[,c(1:3)], datamatrix)
+datamatrix$NAPercent <- (rowSums(is.na(datamatrix))/max(rowSums(is.na(datamatrix))))*100
 
-datamatrix <- datamatrix[,c(2:3,(ncol(datamatrix)-1):ncol(datamatrix))]
+datamatrix <- datamatrix[,c(2:3,(ncol(datamatrix)-2):ncol(datamatrix))]
 return(datamatrix)
 
 }
@@ -190,15 +221,17 @@ ServicesData_MaxMin <- MaxMin_Fun(ServicesData_NoNAs, "Services")
 TransportData_MaxMin <- MaxMin_Fun(TransportData_NoNAs, "Transport")
 
 ##For Housing data, only one column and already 0 to 1
-colnames(HousingData_NoNAs) <- c("iso2c", "country", "year", "MaxMin_Index")
+colnames(HousingData_NoNAs) <- c("iso2c", "country", "MaxMin_Index", "year")
 HousingData_NoNAs$MaxMin_Index <- HousingData_NoNAs$MaxMin_Index/100
 HousingData_NoNAs$CLUM_category <- "Housing"
-HousingData_MaxMin <- HousingData_NoNAs[,c(2:5)]
+HousingData_NoNAs$NAPercent <- (rowSums(is.na(HousingData_NoNAs))/max(rowSums(is.na(HousingData_NoNAs))))*100
+HousingData_MaxMin <- HousingData_NoNAs[,c(2:6)]
 
 ##For Goods Data, just rename column
 colnames(GoodsData) <- c("country", "year", "MaxMin_Index")
 GoodsData$MaxMin_Index <- GoodsData$MaxMin_Index/100
 GoodsData$CLUM_category <- "Goods"
+GoodsData$NAPercent <- (rowSums(is.na(GoodsData))/max(rowSums(is.na(GoodsData))))*100
 GoodsData_MaxMin <- GoodsData
 
 ##Binding Data together for single spreadsheet
@@ -240,7 +273,7 @@ TransportData_ZScore <- ZScore_Fun(TransportData_NoNAs, "Transport")
 
 ##For Housing data, only one column and already 0 to 1
 ZScore_Index <- scale(HousingData_NoNAs$MaxMin_Index)
-HousingData_ZScore <- cbind(HousingData_NoNAs[,c(2:3, 5)], ZScore_Index)
+HousingData_ZScore <- cbind(HousingData_NoNAs[,c(2, 4:5)], ZScore_Index)
 
 ##For Goods Data, just rename column
 ZScore_Index <- scale(GoodsData$MaxMin_Index)
